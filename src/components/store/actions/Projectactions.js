fb869d33885f1998fd63f1837066fc86232f6023
Vishal Thakur <39324895+vishal219@users.noreply.github.com>
2020-08-25 08:40:16 -0700

    Update Projectactions.js
    connect to firestore

--- src/components/store/actions/Projectactions.js
+++ src/components/store/actions/Projectactions.js
@@ -1,9 +1,22 @@
+
 import React from 'react'
 
 export default function createProject(project) {
-    return (dispatch)=>{
+    return (dispatch,getState,{getFirebase,getFirestore})=>{
         //make async call to database
-        dispatch({type:'CREATE_PROJECT',project})
+        const firestore=getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName:'KM',
+            authorLastName:'Priyanka',
+            authorId:12345,
+            createdAt: new Date()
+        }).then(()=>{
+            dispatch({type:'CREATE_PROJECT',project})
+        }).catch(err =>{
+            dispatch({type:'CREATE_PROJECT_ERR',project})
+        })
+        
     }
     
 }
