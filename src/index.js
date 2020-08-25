5acb4ef41f24cd53007ac0bebcdf6a0bea02d9f3
Vishal Thakur <39324895+vishal219@users.noreply.github.com>
2020-08-25 08:08:21 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore,applyMiddleware} from 'redux'
+import {createStore,applyMiddleware,compose} from 'redux'
 import rootReducer from './components/reducers/rootReducer'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbconfig from './config/fbconfig'
 
-const store=createStore(rootReducer,applyMiddleware(thunk))
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbconfig), // redux binding for firebase
+    reduxFirestore(fbconfig) // redux bindings for firestore
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
