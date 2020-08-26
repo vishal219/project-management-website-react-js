ca619a99747f4ab6b9cbbd9321a7eb8c325c34e8
Vishal Thakur <39324895+vishal219@users.noreply.github.com>
2020-08-26 09:20:15 -0700

    Added link for specific project.

--- src/components/projects/Projectlist.js
+++ src/components/projects/Projectlist.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Projectsummary from './Projectsummary'
-
+import {Link} from 'react-router-dom'
 const ProjectList = ({projects}) => {
-
+  
   return (
     <div className="project-list section">  
      {
+       
          projects && projects.map(project=>{
              return(
+               <Link to={"/project/"+ project.id} key={project.id}>
+               
                  <Projectsummary project={project} key={project.id}/>
+                </Link>
              )
          })
      }
